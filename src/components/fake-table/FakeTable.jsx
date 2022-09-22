da9372c96107c45d5e2bfd5c4f04ad0a3c82dbb3
Otajon Murodov <imcodercoder@gmail.com>
2022-09-22 21:58:22 +0500

    Changed some style issues

--- src/components/fake-table/FakeTable.jsx
+++ src/components/fake-table/FakeTable.jsx
@@ -37,34 +37,43 @@ export default function BasicTable({ fakeUsers, loading, error }) {
     )
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Username</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Phone</TableCell>
-            <TableCell align="right">Website</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {fakeUsers?.map((user) => (
-            <TableRow
-              key={user?.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {user?.name}
-              </TableCell>
-              <TableCell align="right">{user?.username}</TableCell>
-              <TableCell align="right">{user?.email}</TableCell>
-              <TableCell align="right">{user?.phone}</TableCell>
-              <TableCell align="right">{user?.website}</TableCell>
+    <div
+      style={{
+        display: "flex",
+        marginTop: "2rem",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Username</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Phone</TableCell>
+              <TableCell align="right">Website</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {fakeUsers?.map((user) => (
+              <TableRow
+                key={user?.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {user?.name}
+                </TableCell>
+                <TableCell align="right">{user?.username}</TableCell>
+                <TableCell align="right">{user?.email}</TableCell>
+                <TableCell align="right">{user?.phone}</TableCell>
+                <TableCell align="right">{user?.website}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   )
 }
